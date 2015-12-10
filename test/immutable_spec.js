09665f8d08d4a4221f1a8ae73b2b24882234defa
Evan Turner <evturn@gmail.com>
2015-12-10 17:57:33 -0500

    Create test for Immutability

---
+++ test/immutable_spec.js
@@ -0,0 +1,35 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a tree', () => {
+
+    function addPlayer(currentState, player) {
+      return currentState.update('players', (players) => players.push(player));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        players: List.of('Magic Johnson', 'Dominique Wilkins')
+      });
+      let nextState = addPlayer(state, 'Clyde Drexler');
+
+      expect(nextState).to.equal(Map({
+        players: List.of(
+          'Magic Johnson',
+          'Dominique Wilkins',
+          'Clyde Drexler'
+        )
+      }));
+      expect(state).to.equal(Map({
+        players: List.of(
+          'Magic Johnson',
+          'Dominique Wilkins'
+        )
+      }));
+    });
+
+  });
+
+});
