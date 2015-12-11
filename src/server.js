ff2e0610736de0fde8f4bf3f77c6f4d96394aa29
Evan Turner <evturn@gmail.com>
2015-12-11 01:13:03 -0500

    Add entries json file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 'use strict';
 import Server from 'socket.io';
 
-export default function startServer() {
+export function startServer() {
   const io = new Server().attach(8090);
 
   store.subscribe(
